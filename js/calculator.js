38e2211203404cc1990c5a20adcebf1a2ff80f9f
Tomas Rafanavicius <tomas_raf@yahoo.com>
2019-09-10 15:36:18 +0300

    Sudėtis, Atimtis, Daugyba, Dalyba

--- js/calculator.js
+++ js/calculator.js
@@ -14,11 +14,47 @@ function dalinti(a, b) {
     return a / b;
 }
 
-$('[value=Dauginti]').click(function () {
-    let a = $('#fval').val();
-    let b = $('#sval').val();
-    console.log(a, b);
+$(document).ready(function () {
+    $('#jid_main_content').on( "mainContentChanged", function( event, param1 ) {
+        if (param1 == 'kontaktai') {
+            $('input[value*=Dauginti]').click(function () {
+                let a = parseFloat($('#fval').val());
+                let b = parseFloat($('#sval').val());
+                console.log(a, b);
 
-    let result = dauginti(a, b);
-    $('#rez span').html(result);
+                let result = dauginti(a, b);
+                $('.jid_res > span').html(isNaN(result) ? 'Check values\' formats': result);
+            });
+
+            $('input[value*=Dalinti]').click(function () {
+                let a = parseFloat($('#fval').val());
+                let b = parseFloat($('#sval').val());
+                console.log(a, b);
+
+                let result = dalinti(a, b);
+                $('.jid_res > span').html(isNaN(result) ? 'Check values\' formats': result);
+            });
+
+            $('input[value*=Sudeti]').click(function () {
+                let a = parseFloat($('#fval').val());
+                let b = parseFloat($('#sval').val());
+                console.log(a, b);
+
+                let result = sudeti(a, b);
+                $('.jid_res > span').html(isNaN(result) ? 'Check values\' formats': result);
+            });
+
+            $('input[value*=Atimti]').click(function () {
+                let a = parseFloat($('#fval').val());
+                let b = parseFloat($('#sval').val());
+                console.log(a, b);
+
+                let result = atimti(a, b);
+                $('.jid_res > span').html(isNaN(result) ? 'Check values\' formats': result);
+            });
+        }
+    });
+
+
+
 });
--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,19 @@
 $(document).ready(function () {
     // Load initial page
-    $.get( '/about.html', function( data ) {
-        $( '#jid_content' ).html( data );
+    $.get( '/calculator.html', function( data ) {
+        let mainContent = $( '#jid_main_content' );
+        mainContent.html( data );
+        let navbar = $( '.navbar a' );
+        navbar.removeClass('active');
+        $(".navbar a[href*='kontaktai']").addClass('active');
+        //mainContent.addClass('jid_apie');
+        mainContent.trigger( "mainContentChanged", [ "kontaktai" ] );
     });
 
 
     $( '.navbar a' ).click(function () {
         let self = $(this);
+        let mainContent = $( '#jid_main_content' );
         let isActive = self.hasClass('active');
         if ( !isActive ) {
             $( '.navbar a' ).removeClass('active');
@@ -14,22 +21,33 @@ $(document).ready(function () {
 
             if (self.attr('href') === '#apie') {
                 $.get( '/about.html', function( data ) {
-                    $( '#jid_content' ).html( data );
+                    mainContent.html( data );
+                    //mainContent.removeClass('jid_kontaktai', 'jid_portfolio');
+                    //mainContent.addClass('jid_apie');
+                    mainContent.trigger( "mainContentChanged", [ "apie" ] );
                 });
 
                 //return;
             }
             if (self.attr('href') === '#kontaktai') {
                 $.get( '/calculator.html', function( data ) {
-                    $( '#jid_content' ).html( data );
+                    mainContent.html( data );
+                    //mainContent.removeClass('jid_apie', 'jid_portfolio');
+                    //mainContent.addClass('jid_kontaktai');
+                    mainContent.trigger( "mainContentChanged", [ "kontaktai" ] );
                 });
 
                 return;
             }
             if (self.attr('href') === '#portfolio') {
+                //mainContent.removeClass('jid_portfolio');
+                //mainContent.addClass('jid_portfolio');
+                mainContent.trigger( "mainContentChanged", [ "portfolio" ] );
 
                 return;
             }
+
+
 
         }
 
